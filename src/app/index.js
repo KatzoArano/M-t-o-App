244fed3be0fff12f5415a45115981c444a20451a
oarano <oarano@macbook-pro-de-oarano.home>
2020-03-11 09:51:59 +0100

    create class UI

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,14 @@
 const {Weather} = require('./Weather');
+const {Ui} = require('./Ui');
+
 require('./index.css');
 
+const ui = new Ui();
 const weather = new Weather('Briscous', 'fr');
 
 async function fetchWeather(){
 const data = await weather.getWeather();
-console.log(data)
+ui.render(data);
 }
 
 document.addEventListener('DOMContentLoaded', fetchWeather());
